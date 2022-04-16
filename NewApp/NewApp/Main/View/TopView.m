f15e6d40959780c64461bdf13e028ca01193fb2d
Cheesetunafish <1614312228@qq.com>
2022-04-16 23:32:13 +0800

    布局修改

--- NewApp/NewApp/Main/View/TopView.m
+++ NewApp/NewApp/Main/View/TopView.m
@@ -6,8 +6,11 @@
 //
 
 #import "TopView.h"
+
 #import "UIView+Frame.h"
 #import "NSDate+Day.h"
+#import "Masonry.h"
+
 @implementation TopView
 - (instancetype)initWithFrame:(CGRect)frame {
     self = [super initWithFrame:frame];
@@ -22,6 +25,8 @@
         [self addSubview:self.dayNumber];
         [self addSubview:self.line2];
         [self addSubview:self.dayDate];
+        
+        [self setPosition];
     }
     return self;
 }
@@ -29,10 +34,6 @@
 - (UIButton *)personalBtn {
     if (!_personalBtn) {
         _personalBtn = [[UIButton alloc] init];
-        _personalBtn.width = _personalBtn.height = 50;
-        _personalBtn.left = self.left + 10;
-        _personalBtn.bottom = self.top + 8;
-        
         _personalBtn.layer.masksToBounds = YES;
         _personalBtn.layer.cornerRadius = 25;
         
@@ -46,10 +47,12 @@
         _nameLbl = [[UILabel alloc] init];
         _nameLbl.text = @"用户名";
 
-        [_nameLbl stretchLeft_toPointX:self.personalBtn.right offset:20];
-        [_nameLbl stretchTop_toPointY:self.top offset:-25];
-        _nameLbl.width = 70;
-        _nameLbl.height = 20;
+//        [_nameLbl stretchLeft_toPointX:self.personalBtn.right offset:20];
+//        [_nameLbl stretchTop_toPointY:self.top offset:-25];
+        
+        
+//        _nameLbl.width = 70;
+//        _nameLbl.height = 20;
         
         _nameLbl.font = [UIFont systemFontOfSize:20];
         _nameLbl.textColor = [UIColor greenColor];
@@ -62,11 +65,12 @@
     if (!_line1) {
         _line1 = [[UILabel alloc] init];
         _line1.text = @"|";
-        _line1.textColor = [UIColor colorWithRed:0.8 green:0.8 blue:0.8 alpha:1];
-        [_line1 stretchLeft_toPointX:self.nameLbl.right offset:2];
-        [_line1 stretchTop_toPointY:self.top offset:-26];
-        _line1.width = 3;
-        _line1.font = [UIFont systemFontOfSize:25];
+        _line1.textColor = [UIColor whiteColor];
+//        [_line1 stretchLeft_toPointX:self.nameLbl.right offset:2];
+//        [_line1 stretchTop_toPointY:self.top offset:-26];
+//        _line1.width = 3;
+        
+        _line1.font = [UIFont systemFontOfSize:40];
         
     }
     return _line1;
@@ -78,9 +82,10 @@
         _monthNumber.text = @"xx月";
         _monthNumber.textColor = [UIColor greenColor];
         
-        [_monthNumber stretchLeft_toPointX:self.line1.right offset:15];
-        [_monthNumber stretchTop_toPointY:self.top offset:-30];
-        _monthNumber.width = 40;
+//        [_monthNumber stretchLeft_toPointX:self.line1.right offset:15];
+//        [_monthNumber stretchTop_toPointY:self.top offset:-30];
+//        _monthNumber.width = 40;
+        
         _monthNumber.font = [UIFont systemFontOfSize:16];
     }
     return _monthNumber;
@@ -92,9 +97,10 @@
         _dayNumber.text = @"xx日";
         _dayNumber.textColor = [UIColor greenColor];
         
-        [_dayNumber stretchLeft_toPointX:self.line1.right offset:15];
-        [_dayNumber stretchTop_toPointY:self.top offset:-20];
-        _dayNumber.width = 40;
+//        [_dayNumber stretchLeft_toPointX:self.line1.right offset:15];
+//        [_dayNumber stretchTop_toPointY:self.top offset:-20];
+//        _dayNumber.width = 40;
+        
         _dayNumber.font = [UIFont systemFontOfSize:16];
     }
     return _dayNumber;
@@ -104,12 +110,13 @@
     if (!_line2) {
         _line2 = [[UILabel alloc] init];
         _line2.text = @"|";
-        _line2.textColor = [UIColor colorWithRed:0.8 green:0.8 blue:0.8 alpha:1];
+        _line2.textColor = [UIColor whiteColor];
         
-        [_line2 stretchLeft_toPointX:self.monthNumber.right offset:5];
-        [_line2 stretchTop_toPointY:self.top offset:-26];
-        _line2.width = 3;
-        _line2.font = [UIFont systemFontOfSize:25];
+//        [_line2 stretchLeft_toPointX:self.monthNumber.right offset:5];
+//        [_line2 stretchTop_toPointY:self.top offset:-26];
+//        _line2.width = 3;
+        
+        _line2.font = [UIFont systemFontOfSize:40];
     }
     return _line2;
 }
@@ -120,14 +127,58 @@
         _dayDate.text = @"星期x";
         _dayDate.textColor = [UIColor greenColor];
                               
-        [_dayDate stretchLeft_toPointX:self.line2.right offset:15];
-        [_dayDate stretchTop_toPointY:self.top offset:-25];
-        _dayDate.width = 70;
-        _dayDate.height = 20;
+//        [_dayDate stretchLeft_toPointX:self.line2.right offset:15];
+//        [_dayDate stretchTop_toPointY:self.top offset:-25];
+//        _dayDate.width = 70;
+//        _dayDate.height = 20;
+        
         _dayDate.font = [UIFont systemFontOfSize:20];
     }
     return _dayDate;
 }
+
+
+- (void)setPosition {
+    [self.personalBtn mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerY.equalTo(self);
+        make.left.equalTo(self).offset(5);
+        make.size.mas_equalTo(CGSizeMake(50, 50));
+    }];
+    
+    [self.nameLbl mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.bottom.equalTo(self).offset(-15);
+        make.left.equalTo(self.personalBtn).offset(60);
+    }];
+    
+    [self.line1 mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.width.mas_equalTo(4);
+        make.left.equalTo(self.nameLbl.mas_right).offset(10);
+        make.bottom.equalTo(self).offset(-5);
+    }];
+    
+    
+    [self.monthNumber mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.bottom.equalTo(self).offset(-30);
+        make.left.equalTo(self.nameLbl).offset(90);
+    }];
+    
+    [self.dayNumber mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(self.monthNumber);
+        make.bottom.equalTo(self.monthNumber).offset(20);
+    }];
+    
+    [self.line2 mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.width.mas_equalTo(4);
+        make.left.equalTo(self.monthNumber.mas_right).offset(10);
+        make.bottom.equalTo(self).offset(-5);
+    }];
+    
+    [self.dayDate mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.bottom.equalTo(self).offset(-15);
+            make.left.equalTo(self.line2).offset(20);
+    }];
+}
+
 
 /*
 // Only override drawRect: if you perform custom drawing.
