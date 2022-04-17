e77def09aa59121e322c6c60f34e3b6017ecde85
SungKai <2256047981@qq.com>
2022-04-17 09:43:05 +0800

    [feature]初始化SN模型

---
+++ NewApp/NewApp/SNModel/SNModel.h
@@ -0,0 +1,20 @@
+//
+//  SNModel.h
+//  NewApp
+//
+//  Created by 宋开开 on 2022/4/16.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SNModel : NSObject
+
+@property (nonatomic, copy) NSString *title;
+
+@property (nonatomic, copy) NSString *text;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ NewApp/NewApp/SNModel/SNModel.m
@@ -0,0 +1,12 @@
+//
+//  SNModel.m
+//  NewApp
+//
+//  Created by 宋开开 on 2022/4/16.
+//
+
+#import "SNModel.h"
+
+@implementation SNModel
+
+@end
