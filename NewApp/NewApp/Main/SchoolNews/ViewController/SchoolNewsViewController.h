c78b5b60bd245f1d87ab6946f53c6c78f553a51c
SungKai <2256047981@qq.com>
2022-04-23 10:58:54 +0800

    [optimize]解决冲突

--- NewApp/NewApp/Main/SchoolNews/ViewController/SchoolNewsViewController.h
+++ NewApp/NewApp/Main/SchoolNews/ViewController/SchoolNewsViewController.h
@@ -5,11 +5,24 @@
 //  Created by 宋开开 on 2022/4/9.
 //
 
-#import <UIKit/UIKit.h>
 //教务新闻
-NS_ASSUME_NONNULL_BEGIN
+#import <UIKit/UIKit.h>
+//VC
+#import "SNNextViewController.h"
 
+NS_ASSUME_NONNULL_BEGIN
+@protocol SchoolNewsViewControllerDelegete <NSObject>
+@required
+/// 跳转到教务新闻详情页
+/// @param snNextVC 要跳转的VC
+- (void)jumpToNextVC:(SNNextViewController *)snNextVC;
+
+///从nextVC跳回frontVC
+- (void)jumpToFrontVC;
+@end
 @interface SchoolNewsViewController : UIViewController
+
+@property (nonatomic, weak) id <SchoolNewsViewControllerDelegete> SchoolNewsViewControllerDelegete;
 
 @end
 
--- NewApp/NewApp/Main/SchoolNews/ViewController/SchoolNewsViewController.m
+++ NewApp/NewApp/Main/SchoolNews/ViewController/SchoolNewsViewController.m
@@ -6,13 +6,21 @@
 //
 
 #import "SchoolNewsViewController.h"
-
+//VC
+#import "SNNextViewController.h"
 //V
 #import "SchoolNewsTableView.h"
+//M
+#import "SNModel.h"
 
-@interface SchoolNewsViewController ()
+@interface SchoolNewsViewController () <
+    SchoolNewsTableViewDelegate,
+    SNNextViewControllerDelegete
+>
 
 @property (nonatomic, strong) SchoolNewsTableView *tableView;
+
+@property (nonatomic, strong) NSArray<SNModel *> *dataArray;
 
 @end
 
@@ -23,19 +31,54 @@
     self.view.backgroundColor = [UIColor systemBlueColor];
     // Do any additional setup after loading the view.
     [self.view addSubview:self.tableView];
+    //传递数据给View
+    self.tableView.SNData = self.dataArray;
+
+//    //把数据传递给SNNextVC
+//    [self.snFrontDelegete gainArrayDataToNext:self.dataArray];
 }
 
-#pragma mark-懒加载
+#pragma mark - Getter
 - (SchoolNewsTableView *)tableView {
     if (!_tableView) {
         _tableView = [[SchoolNewsTableView alloc] initWithFrame:CGRectMake(0, 0, ScreenWidth, ScreenHeight) style:UITableViewStyleGrouped];
-        _tableView.backgroundColor = [UIColor colorNamed:@"237_237_237"];
+        _tableView.backgroundColor = [UIColor lightGrayColor];
+        _tableView.schoolDelegate = self;
     }
     return _tableView;
 }
+- (NSArray *)dataArray {
+    if (!_dataArray) {
+        //获取plist文件的路径
+        NSString *path = [[NSBundle mainBundle] pathForResource:@"schoolNews.plist" ofType:nil];
+        //根据文件路径获取plist文件
+        NSArray *arrryDicts = [NSArray arrayWithContentsOfFile:path];
+        NSMutableArray *maArray = [NSMutableArray array];
+        for (NSDictionary *dic in arrryDicts) {
+            SNModel *snModel = [[SNModel alloc] init];
+            [snModel SNModelWithDic:dic];
+            [maArray addObject:snModel];
+        }
+        _dataArray = maArray;
+        NSLog(@"=============%ld", _dataArray.count);
+    }
+    return _dataArray;
+}
+#pragma mark - Delegete
+
+// MARK:  <SchoolNewsTableViewDelegate>
+- (void)clickGainIndexPath:(NSIndexPath *)indexPath {
+    //传给下一个VC该新闻所在的index
+    SNNextViewController *snNextVC = [[SNNextViewController alloc] initWithTitleLab:self.dataArray[indexPath.row].title TextLab:self.dataArray[indexPath.row].text];
+    snNextVC.SNNextViewControllerDelegete = self;
+    [self.SchoolNewsViewControllerDelegete jumpToNextVC:snNextVC];
+}
+#pragma mark - <SNNextViewControllerDelegete>
+- (void)back {
+    [self.SchoolNewsViewControllerDelegete jumpToFrontVC];
+}
 /*
 #pragma mark - Navigation
-
 // In a storyboard-based application, you will often want to do a little preparation before navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
     // Get the new view controller using [segue destinationViewController].
