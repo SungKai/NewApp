cd480b8ef2d20b021cffce9ec08b24bcb5a08e42
SungKai <2256047981@qq.com>
2022-04-23 10:52:01 +0800

    [optimize]解决冲突

--- NewApp/NewApp/Main/SchoolNews/View/SchoolNewsTableView/cell/SchoolNewsCell.m
+++ NewApp/NewApp/Main/SchoolNews/View/SchoolNewsTableView/cell/SchoolNewsCell.m
@@ -6,7 +6,9 @@
 //
 
 #import "SchoolNewsCell.h"
+//Tool
 #import "Masonry.h"
+#import "UILabel+AutoHeight.h"
 
 @implementation SchoolNewsCell
 
@@ -14,82 +16,60 @@
 - (instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier{
     self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
     if (self) {
-        self.backgroundColor = [UIColor colorNamed:@"247_247_247"];
+        self.backgroundColor = [UIColor whiteColor];
         [self.contentView addSubview:self.titleLab];
-//        [self.contentView addSubview:self.hintLab];
-        [self setPosition];
-        
-        self.layer.cornerRadius=10.0f;
-        self.layer.masksToBounds=YES;
+        self.layer.cornerRadius = 10.0f;
+        self.layer.masksToBounds = YES;
         [self setFrame:CGRectMake(0, 0, ScreenWidth, 0)];
     }
     return self;
 }
 
+#pragma mark - Method
 /// 复用池
 + (SchoolNewsCell *)CreateReusableCell:(UITableView *)tableView {
     static NSString *identyfing = @"cell";
     SchoolNewsCell *schoolCell = [tableView dequeueReusableCellWithIdentifier:identyfing];
     if (!schoolCell) {
         schoolCell = [[SchoolNewsCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:identyfing];
-        schoolCell.titleLab.backgroundColor = [UIColor colorNamed:@"204_204_204"];
+        schoolCell.titleLab.backgroundColor = [UIColor lightGrayColor];
 //        schoolCell.hintLab.backgroundColor = [UIColor systemGreenColor];
     }
     return schoolCell;
 }
-
+//载入信息
 - (SchoolNewsCell *)cellWithInformation:(SchoolNewsCell *)cell WithTitleText:(NSString *)titleText WithHintText:(NSString *)hintText {
     cell.titleLab.text = titleText;
-//    cell.hintLab.text = hintText;
     cell.titleLab.backgroundColor = [UIColor clearColor];
     cell.hintLab.backgroundColor = [UIColor clearColor];
-
+    [cell calculateTitle:cell AndTitleText:titleText];
     return cell;
 }
 
+//设置title行数
+- (void)calculateTitle:(SchoolNewsCell *)cell AndTitleText:(NSString *)str{
+    CGRect tempTitleFrame = cell.titleLab.frame;
+    CGFloat width = ScreenWidth - 2 * 10;
+    //得到文字相应的size
+    tempTitleFrame.size = [cell.titleLab MaxLabelWidth:str FontOfSize:20 MaxWidth:width  MaxNumberOfLine:2 Interval:3];
+    tempTitleFrame.origin = CGPointMake(10, 4);
+    cell.titleLab.frame = tempTitleFrame;
+}
 //设置分割线的宽度和高度
 -(void)setFrame:(CGRect)frame
 {
-  frame.origin.x =2;//这里间距为10可以根据自己的情况调整
-  frame.size.width -=frame.origin.x;
+  frame.origin.x = 2;//这里间距为10可以根据自己的情况调整
+  frame.size.width -= frame.origin.x;
   frame.size.height -= 5 * frame.origin.x;
   [super setFrame:frame];
 }
-
 #pragma mark- 懒加载
 - (UILabel *)titleLab {
     if (!_titleLab) {
         _titleLab = [[UILabel alloc] init];
         _titleLab.textColor = [UIColor blackColor];
-        _titleLab.font = [UIFont boldSystemFontOfSize:26];
-        _titleLab.text = @"教务在线";
+        _titleLab.font = [UIFont boldSystemFontOfSize:20];
     }
     return _titleLab;
 }
-
-//- (UILabel *)hintLab {
-//    if (!_hintLab) {
-//        _hintLab = [[UILabel alloc] init];
-//        _hintLab.textColor = [UIColor blackColor];
-//        _hintLab.textColor = [UIColor systemGrayColor];
-//        _hintLab.font = [UIFont systemFontOfSize:15];
-//
-//        _hintLab.text = @"hintTitle";
-//    }
-//    return _hintLab;
-//}
-
-- (void)setPosition {
-    
-    [self.titleLab mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.left.equalTo(self.contentView).offset(20);
-        make.top.equalTo(self.contentView).offset(15);
-    }];
-    
-//    [self.hintLab mas_makeConstraints:^(MASConstraintMaker *make) {
-//        make.left.equalTo(self.titleLab);
-//        make.top.equalTo(self.titleLab.mas_bottom).offset(5);
-//    }];
-}
-
 @end
